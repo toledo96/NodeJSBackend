4796eb5042ec59fac6a4c994e75185fe896fa603
Andres <atm1996.ast@gmail.com>
2023-05-03 11:05:20 -0600

    correccion de código

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,56 @@
+import {User} from "../models/User.js"
+
+export const register = async(req,res) => {
+    const {email,password} = req.body
 
 
-export const register = (req,res) => {
+    try {
+        //validar datos repetidos por email alternativa
+        let user = await User.findOne({email})
+        // if(user) throw {code:11000}
+        if(user)  return res.status(400).json({error: "Ya existe este usuario"})
+        
+        user = new User({email,password})
+        await user.save()
 
+        //falta jwt
 
-    console.log(req.body)
-    res.json({ok:true});
+        return res.status(201).json({
+            ok:true
+        })
+    } catch (error) {
+        console.log(error.code)
+        //alternativa validar datos repetidos por defecto mongoose
+        // if(error.code === 11000){
+        //     return res.status(400).json({error: "Ya existe este usuario"})
+        // }
+    }
+    return res.status(500).json({error: "Error de servidor"})
+
 }
 
-export const login = (req,res) => {
-    res.json({ok:true});
-}
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        let user = await User.findOne({ email });
+        if (!user)
+            return res.status(403).json({ error: "No existe este usuario" });
+
+        const respuestaPassword = await user.comparePassword(password);
+        if (!respuestaPassword)
+            return res.status(403).json({ error: "Contraseña incorrecta" });
+
+        // Generar el token JWT
+
+
+        return res.status(201).json({
+            ok:true
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Error de servidor" });
+    }
+};
 
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 
 //Routes in routes
-app.use('/api/v1',authRouter);
+app.use('/api/v1/auth',authRouter);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log("hola"))
